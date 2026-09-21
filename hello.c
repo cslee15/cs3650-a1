7d9123260897c39433f4bed1bb3d96cdb314f623
Catherine Lee <lee.cat@northeastern.edu>
2026-09-21 00:29:00 -0400

    Add for loop to print name and email 20 times for Part 4

--- hello.c
+++ hello.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Catherine\n");
-  printf("lee.cat@northeastern.edu\n");
-
+  for (int i = 0; i < 20; i++) {
+    printf("Catherine\n");
+    printf("lee.cat@northeastern.edu\n");
+  }
   return 0;
 }
